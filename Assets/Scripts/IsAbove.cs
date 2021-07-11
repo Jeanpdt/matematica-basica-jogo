df46bf371023ed423831fd6dc5e78786b4ed0cf1
Jeanpdt <rodrigo.santospdt@gmail.com>
2021-07-11 18:30:53 -0400

    first commit

---
+++ Assets/Scripts/IsAbove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsInBox : MonoBehaviour
+{    
+    public bool isInBox;
+
+    public int qtdObjectsInBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update() {
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Object")) {
+            qtdObjectsInBox += 1;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Object")) {
+             qtdObjectsInBox -= 1;
+        }
+    }
+}
---
+++ Assets/Scripts/IsInBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsAbove : MonoBehaviour
+{    public bool isInBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+   void Update(){
+    if(isInBox){
+    } else {
+    }
+}
+    void OnTriggerStay(Collider other) {
+        if(other.CompareTag("Player")) {
+            isInBox = true;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag("Player")) {
+            isInBox = false;
+        }
+    }
+}
