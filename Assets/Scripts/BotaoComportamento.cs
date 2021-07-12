d00e2432afed14a2ea00d9a61fab533de5f964c0
Jeanpdt <rodrigo.santospdt@gmail.com>
2021-07-12 06:03:10 -0400

    add reference of variable to button

--- Assets/Scripts/BotaoComportamento.cs
+++ Assets/Scripts/BotaoComportamento.cs
@@ -1,11 +1,20 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BotaoComportamento : MonoBehaviour
 {
-    public int n;
-    public void OnButtonPress(){
-        n++;
+    public GameObject recipiente;
+
+    void Start()
+    {
+        recipiente = GameObject.Find("Counter");
+    }
+
+    public void OnButtonPress()
+    {
+        BoxObjectCounter boxObjectCounter = recipiente.GetComponent<BoxObjectCounter>();
+
+        Debug.Log(boxObjectCounter.qtdObjectsInBox);
     }
 }
